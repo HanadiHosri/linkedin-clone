232448e3fd6a89a3128bd4ed4d547b78c7e6f7e3
unknown <hanadi.hosri@gmail.com>
2024-03-24 19:57:13 +0200

    implementing signin api

--- backend/authentication/signin.php
+++ backend/authentication/signin.php
@@ -1,2 +1,28 @@
 <?php
 include("../connection.php");
+
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$query = $mysqli->prepare('select email, password from users where email=?');
+$query->bind_param('s', $email);
+$query->execute();
+$query->store_result();
+$query->bind_result($email, $hashed_password);
+$query->fetch();
+$num_rows = $query->num_rows();
+
+if ($num_rows == 0) {
+    $response['status'] = "user not found";
+} else {
+    if (password_verify($password, $hashed_password)) {
+        $response['status'] = "logged in";
+        $response['email'] = $email;
+    } else {
+        $response['status'] = "incorrect credentials";
+    }
+}
+
+echo json_encode($response);
