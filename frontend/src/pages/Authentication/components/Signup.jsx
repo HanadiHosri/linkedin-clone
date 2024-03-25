2b9665d545fe33e82d397df5660e17b0d349fce1
unknown <hanadi.hosri@gmail.com>
2024-03-25 07:37:52 +0200

    starting to style sign up form

--- frontend/src/pages/Authentication/components/Signup.jsx
+++ frontend/src/pages/Authentication/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import "../style.css";
 
 const Signup = () => {
     const [credentials, setCredentials] = useState({email: "", password: "" });
@@ -6,47 +7,55 @@ const Signup = () => {
     return(
         <div>
             <div>
-                <header>
+                <header className="header">
                     <div><p>logo</p></div>
                     <h1>Make the most of your professional life</h1>
                 </header>
                 <div>
-                    <form>
-                        <label htmlFor="email">Email</label>
-                        <input 
-                        type="text" 
-                        placeholder="Email" 
-                        onChange={(e) => {
-                            setCredentials({
-                                ...credentials,
-                                email: e.target.value,
-                            });
-                        }}
-                        />
-                        <label htmlFor="password">Password</label>
-                        <input 
-                        type="password"
-                        placeholder="**********"
-                        onChange={(e) => {
-                            setCredentials({
-                                ...credentials,
-                                password: e.target.value,
-                            });
-                        }}
-                        />
-                        <label htmlFor="type">Type</label>
-                        <select
-                        name="type"
-                        onChange={(e) => {
-                            setCredentials({
-                                ...credentials,
-                                type: e.target.value,
-                            });
-                        }}
-                        >
-                            <option value="user">User</option>
-                            <option value="company">Company</option>
-                        </select>
+                    <form className="form flex column center">
+                        <div className="flex column">
+                            <label htmlFor="email">Email</label>
+                            <input 
+                            type="text" 
+                            placeholder="Email" 
+                            onChange={(e) => {
+                                setCredentials({
+                                    ...credentials,
+                                    email: e.target.value,
+                                });
+                            }}
+                            />
+                        </div>
+                        <div className="flex column">
+                            <label htmlFor="password">Password</label>
+                            <input 
+                            type="password"
+                            placeholder="**********"
+                            onChange={(e) => {
+                                setCredentials({
+                                    ...credentials,
+                                    password: e.target.value,
+                                });
+                            }}
+                            />
+                        </div>
+                        <div className="flex column">
+                            <label htmlFor="type">Type</label>
+                            <select
+                            name="type"
+                            onChange={(e) => {
+                                setCredentials({
+                                    ...credentials,
+                                    type: e.target.value,
+                                });
+                            }}
+                            >
+                                <option value="user">User</option>
+                                <option value="company">Company</option>
+                            </select>
+                        </div>
+                        
+
                         <button>Agree & Join</button>
 
                     </form>
