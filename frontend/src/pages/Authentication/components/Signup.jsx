124a12e1b0606d7e49f2f6e859ccca74820f893d
unknown <hanadi.hosri@gmail.com>
2024-03-25 07:02:19 +0200

    implementing Signup component

--- frontend/src/pages/Authentication/components/Signup.jsx
+++ frontend/src/pages/Authentication/components/Signup.jsx
@@ -1,10 +1,58 @@
-
+import React, {useState} from "react";
 
 const Signup = () => {
-
+    const [credentials, setCredentials] = useState({email: "", password: "" });
 
     return(
-        <h1>signup</h1>
+        <div>
+            <div>
+                <header>
+                    <div><p>logo</p></div>
+                    <h1>Make the most of your professional life</h1>
+                </header>
+                <div>
+                    <form>
+                        <label htmlFor="email">Email</label>
+                        <input 
+                        type="text" 
+                        placeholder="Email" 
+                        onChange={(e) => {
+                            setCredentials({
+                                ...credentials,
+                                email: e.target.value,
+                            });
+                        }}
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input 
+                        type="password"
+                        placeholder="**********"
+                        onChange={(e) => {
+                            setCredentials({
+                                ...credentials,
+                                password: e.target.value,
+                            });
+                        }}
+                        />
+                        <label htmlFor="type">Type</label>
+                        <select
+                        name="type"
+                        onChange={(e) => {
+                            setCredentials({
+                                ...credentials,
+                                type: e.target.value,
+                            });
+                        }}
+                        >
+                            <option value="user">User</option>
+                            <option value="company">Company</option>
+                        </select>
+                        <button>Agree & Join</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
